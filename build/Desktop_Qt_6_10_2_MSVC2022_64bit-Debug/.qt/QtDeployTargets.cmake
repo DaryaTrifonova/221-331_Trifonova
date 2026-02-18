296a6e1ab7d1508d60275cf5ca8679ec61137862
Трифонова Дарья <trifonusha@gmail.com>
2026-02-18 15:23:23 +0300

    chore: add gitignore and remove build artifacts

--- build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Trifonova_FILE D:/Study/Trifonova/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/Trifonova.exe)
-set(__QT_DEPLOY_TARGET_Trifonova_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Trifonova_RUNTIME_DLLS D:/Qt1/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt1/6.10.2/msvc2022_64/bin/Qt6Guid.dll;D:/Qt1/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
